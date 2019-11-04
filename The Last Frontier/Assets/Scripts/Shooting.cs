e7b8069d6d00eb3638e45011e9cc96c55b579b53
CatbirdBasil <catbirdbasil@yahoo.com>
2019-11-04 13:13:39 +0200

    Deleted test project. Returned meta files

--- The Last Frontier/Assets/Scripts/Shooting.cs
+++ The Last Frontier/Assets/Scripts/Shooting.cs
@@ -1,12 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using ModestTree;
 using UnityEngine;
+
+using TLFGameLogic;
+using TLFGameLogic.Model;
+using Zenject;
 
 public class Shooting : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bulletPrefab;
 
+    [Inject]
+    private CurrentCannonLoadoutProvider loadoutProvider;
+    
+    [Inject]
+    private ILevelInfoProvider levelInfoProvider;
+    
     public float bulletForce = 20f;
 
     void Update()
@@ -22,5 +34,21 @@ public class Shooting : MonoBehaviour
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(firePoint.right * bulletForce, ForceMode2D.Impulse);
+
+        //CurrentCannonLoadout loadout = loadoutProvider.GetLoadout();
+        //Debug.Log("Damage = " + loadout.Damage);
+
+        LevelInfo levelInfo = levelInfoProvider.Get(1);
+        Debug.Log("LevelInfo = " + levelInfo.EnemiesSpawnInfo[0].Enemy.Name);
+        Enemy enemy = levelInfo.EnemiesSpawnInfo[0].Enemy;
+        
+        levelInfo.AllEnemiesDead += delegate(object sender, EventArgs args) { Debug.Log("ALL DEAD"); };
+        
+        Debug.Log("HP = " + enemy.Health);
+        enemy.TakeDamage(10f);
+        Debug.Log("HP = " + enemy.Health);
+        enemy.TakeDamage(51f);
+        Debug.Log("HP = " + enemy.Health);
+
     }
 }
