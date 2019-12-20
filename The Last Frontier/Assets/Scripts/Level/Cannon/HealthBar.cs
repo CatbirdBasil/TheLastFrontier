d58a970d41bafe3cba76ae1624e8eb5ec842e104
annatsytsyluik <ann.tsytsyluik@gmail.com>
2019-12-20 19:57:53 +0200

    Added items icon Added Level menu Added Health bar

---
+++ The Last Frontier/Assets/Scripts/Level/Cannon/HealthBar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Level;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+public class HealthBar : MonoBehaviour
+{
+    private float maxHealth;
+    private int minHealth = 0;
+    private float CurrHealth;
+    public Image heathBar;
+    //public  heathBar;
+    private float mCurrentValue;
+    [Inject] private PlayerState _playerState;
+    
+    void Start()
+    {
+        maxHealth = _playerState.CurrentBase.MaxHp;
+    }
+
+    private void SetHeath(float heath)
+    {
+        if (CurrHealth != heath)
+        {
+            CurrHealth = heath;
+            mCurrentValue =  heath / maxHealth;
+            heathBar.fillAmount = mCurrentValue;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetHeath(_playerState.CurrentBase.CurrentHp);
+    }
+}
