b7d32ec87bc44247ea598fbec3a73045986979d5
CatbirdBasil <catbirdbasil@yahoo.com>
2019-12-21 02:10:19 +0200

    Minor UI improvement

--- The Last Frontier/TLFGameLogic/Config/Levels.cs
+++ The Last Frontier/TLFGameLogic/Config/Levels.cs
@@ -6,8 +6,50 @@ namespace TLFGameLogic.Config
 {
     public class Levels
     {
-        private static int _levelAmount = 1;
+        private static Dictionary<int, LevelInfo> _levelInfos = new Dictionary<int, LevelInfo>()
+        {
+            {1, Level1()},
+            {2, Level2()}
+        };
+
         private static LevelInfo Level1()
+        {
+            var levelInfo = new LevelInfo();
+            levelInfo.BigWaveStartTimes = new List<float>
+            {
+                15f
+            };
+
+            levelInfo.EstimatedMaxEnemiesOnScreen = new Dictionary<EnemyType, int>
+            {
+                {EnemyType.SmallSlime, 14}
+            };
+            
+            levelInfo.AddEnemySpawnInfo(3f, SpawnPoint.D, EnemyFactory.CreateEnemy(EnemyType.SmallSlime));
+            
+            levelInfo.AddEnemySpawnInfo(6f, SpawnPoint.C, EnemyFactory.CreateEnemy(EnemyType.SmallSlime));
+            levelInfo.AddEnemySpawnInfo(6f, SpawnPoint.E, EnemyFactory.CreateEnemy(EnemyType.SmallSlime));
+            
+            levelInfo.AddEnemySpawnInfo(9f, SpawnPoint.A, EnemyFactory.CreateEnemy(EnemyType.SmallSlime));
+            levelInfo.AddEnemySpawnInfo(9f, SpawnPoint.D, EnemyFactory.CreateEnemy(EnemyType.SmallSlime));
+            levelInfo.AddEnemySpawnInfo(9f, SpawnPoint.G, EnemyFactory.CreateEnemy(EnemyType.SmallSlime));
+            
+            levelInfo.AddEnemySpawnInfo(11f, SpawnPoint.B, EnemyFactory.CreateEnemy(EnemyType.SmallSlime));
+            levelInfo.AddEnemySpawnInfo(11f, SpawnPoint.F, EnemyFactory.CreateEnemy(EnemyType.SmallSlime));
+            
+            levelInfo.AddEnemySpawnInfo(15f, SpawnPoint.A, EnemyFactory.CreateEnemy(EnemyType.SmallSlime));
+            levelInfo.AddEnemySpawnInfo(15f, SpawnPoint.B, EnemyFactory.CreateEnemy(EnemyType.SmallSlime));
+            levelInfo.AddEnemySpawnInfo(15f, SpawnPoint.D, EnemyFactory.CreateEnemy(EnemyType.SmallSlime));
+            levelInfo.AddEnemySpawnInfo(15f, SpawnPoint.F, EnemyFactory.CreateEnemy(EnemyType.SmallSlime));
+            levelInfo.AddEnemySpawnInfo(15f, SpawnPoint.G, EnemyFactory.CreateEnemy(EnemyType.SmallSlime));
+
+            levelInfo.Reward = 100;
+            levelInfo.LevelNumber = 1;
+
+            return levelInfo;
+        }
+        
+        private static LevelInfo Level2()
         {
             var levelInfo = new LevelInfo();
             levelInfo.BigWaveStartTimes = new List<float>
@@ -38,23 +80,19 @@ namespace TLFGameLogic.Config
             levelInfo.AddEnemySpawnInfo(19f, SpawnPoint.G, EnemyFactory.CreateEnemy(EnemyType.SmallSlime));
 
             levelInfo.Reward = 100;
-            levelInfo.LevelNumber = 1;
+            levelInfo.LevelNumber = 2;
 
             return levelInfo;
         }
 
         public static LevelInfo Get(int level)
         {
-            switch (level)
-            {
-                case 1: return Level1();
-                default: return null;
-            }
+            return _levelInfos[level];
         }
 
         public static int GetLevelAmount()
         {
-            return _levelAmount;
+            return _levelInfos.Count;
         }
     }
 }
